4c19056909758870c4f3ab6f06476250bca38f5b
nabinghosh <www.champaghosha.12345@gmail.com>
2023-04-09 16:30:16 +0530

    added middleware

--- src/componets/Imageupload.js
+++ src/componets/Imageupload.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import { useState } from 'react'
-export default function Imageupload(){
-    const [ image, setImage ]= useState('')
-    function handleImage(e){
-      console.log(e.target.files[0]);
-      console.log("file has been saved");
-    }
-    return(
-    <>
-    <input type="file" name="file" onChange={handleImage}/>
-    <button>Submit </button>
-    {/* <div></div> */}
-    </>
-   
-    );
-} 
+import React, { useState } from 'react';
+
+export default function ImageUpload() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileInput = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    fetch('/public', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => {
+        console.log(response);
+        console.log('File uploaded successfully');
+      })
+      .catch((error) => {
+        console.error(error);
+        console.error('Failed to upload file');
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" name="file" onChange={handleFileInput} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
